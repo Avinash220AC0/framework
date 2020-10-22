35600004dad0aa82e7d1ee30ebe24ec56b0e9d39
Avinash220AC0 <avinashvurrankula@gmail.com>
2020-10-22 20:33:01 +0530

    Test

---
+++ github/src/login/Nativeloginie.java
@@ -0,0 +1,17 @@
+package login;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Nativeloginie {
+	
+			 //Terms and condition
+
+		   
+	 }
